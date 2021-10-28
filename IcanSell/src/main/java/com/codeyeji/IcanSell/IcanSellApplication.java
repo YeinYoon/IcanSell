cc8eba8afdc83bf5d9d4e34224f48f015b4b9e8b
YeinYoon <yeinyun23@gmail.com>
2021-10-28 13:44:39 +0900

    프로젝트 생성

---
+++ IcanSell/src/main/java/com/codeyeji/IcanSell/IcanSellApplication.java
@@ -0,0 +1,13 @@
+package com.codeyeji.IcanSell;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcanSellApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcanSellApplication.class, args);
+	}
+
+}
---
+++ IcanSell/src/test/java/com/codeyeji/IcanSell/IcanSellApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeyeji.IcanSell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcanSellApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
